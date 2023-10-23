bb31eb1bd0693b73cf6e3aaf31d6c66eb61ce196
NComGit <ncomthrowaway@protonmail.com>
2023-10-23 12:51:00 -0400

    Replaced slideshow with BootStrap Carousel, and applied Bootstrap Grid to events

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$artistPic1 = "https://t4.ftcdn.net/jpg/05/24/83/87/360_F_524838701_3kb13tmtR4wNze5cHpfSepAXCOoXwD0d.jpg";
-$artistPic2 = "https://www.shutterstock.com/image-illustration/number-two-polished-golden-object-260nw-371522539.jpg";
-$artistPic3 = "https://cdn.pixabay.com/photo/2015/04/04/19/13/three-706895_1280.jpg";
+$artistPic1 = "https://www.evenko.ca/_uploads/event/51087/featured.jpg?v=1670334813";
+$artistPic2 = "https://www.evenko.ca/_uploads/event/57675/featured.jpg?v=1697740433";
+$artistPic3 = "https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863";
 $artistName1 = "Artist_Name_1";
 $artistName2 = "Artist_Name_2";
 $artistName3 = "Artist_Name_3";
@@ -14,108 +14,143 @@ $artistName3 = "Artist_Name_3";
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/index.css" />
     <title>Playlist Palooza</title>
 </head>
 
 <body>
     <article class="slideshow">
-        <section class="buttons">
-            <button id="backButton"></button>
-            <button id="forwardButton"></button>
-        </section>
-        <section id="slides">
-            <figure>
-                <img id="slide1" style="display: none" src=<?= $artistPic1 ?> alt=<?= $artistName1 . "_Picture" ?> />
-                <figcaption id="slideText1" style="display: none"><?= $artistName1 ?></figcaption>
-            </figure>
-            <figure>
-                <img id="slide2" style="display: none" src=<?= $artistPic2 ?> alt=<?= $artistName2 . "_Picture" ?> />
-                <figcaption id="slideText2" style="display: none"><?= $artistName2 ?></figcaption>
-            </figure>
-            <figure>
-                <img id="slide3" style="display: none" src=<?= $artistPic3 ?> alt=<?= $artistName3 . "_Picture" ?> />
-                <figcaption id="slideText3" style="display: none"><?= $artistName3 ?></figcaption>
-            </figure>
-        </section>
+        <article class="container">
+            <div id="carouselExampleCaptions" class="carousel slide">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="<?= $artistPic1 ?>" class="d-block w-100" alt="..." />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>
+                                Some representative placeholder content for the first slide.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?= $artistPic2 ?>" class="d-block w-100" alt="..." />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>
+                                Some representative placeholder content for the second slide.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="<?= $artistPic3 ?>" class="d-block w-100" alt="..." />
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>
+                                Some representative placeholder content for the third slide.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </article>
     </article>
 
-    <article class="events">
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-        <figure class="event">
-            <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name"></a>
-            <figcaption>
-                <h4>Laval</h4>
-                <h2>Charlotte Cardin</h2>
-                <h4>Feb 8 2023</h4>
-            </figcaption>
-        </figure>
-
+    <article class="container-fluid">
+        <section class="row mt-2">
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+        </section>
+        <section class="row">
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+        </section>
+        <section class="row">
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+            <figure class="col">
+                <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
+                <figcaption>
+                    <h4>Laval</h4>
+                    <h2>Charlotte Cardin</h2>
+                    <h4>Feb 8 2023</h4>
+                </figcaption>
+            </figure>
+        </section>
     </article>
     <script src="javascript/index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
