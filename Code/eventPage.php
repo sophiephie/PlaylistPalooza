fc25ebc0fa1891aa0d45f8dd8393d3853d22dadd
NComGit <ncomthrowaway@protonmail.com>
2023-10-26 23:00:38 -0400

    Fixed styling of header/footer

--- Code/eventPage.php
+++ Code/eventPage.php
@@ -8,15 +8,20 @@ $eventDate = "November 21, 2023";
 <html lang="en">
 
 <head>
+    <link href="image/icon2.png" rel="icon" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css" />
     <link rel="stylesheet" href="css/eventPage.css" />
     <title><?= $artistName . " | Playlist Palooza" ?></title>
 </head>
 
 <body>
-    <article class="event container">
+    <header> <?php include "../includes/header.html"; ?> </header>
+
+    <article style="background-color: #fff0df;" class="event container-fluid">
+
         <article class="row text-center">
             <img src="https://www.evenko.ca/_uploads/event/57675/featured.jpg?v=1697740433" alt="artist" class="container col-md-6 ">
             <section class="eventInfo col-md-6">
@@ -47,9 +52,9 @@ $eventDate = "November 21, 2023";
             </section>
         </article>
 
-        <article class="otherEvent row text-start">
-            <h2>Other Events (or) Other Events by this artist</h2>
+        <article class="otherEvent row container text-start">
             <section class="otherEvents row">
+                <h2>You may also like</h2>
                 <figure class="col">
                     <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863" alt="artist name" width="100%"></a>
                     <figcaption>
@@ -78,8 +83,10 @@ $eventDate = "November 21, 2023";
         </article>
     </article>
 
+    <footer>
+        <?php include "../includes/footer.html"; ?>
+    </footer>
 
-    <script src="../javascript/eventPage.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
