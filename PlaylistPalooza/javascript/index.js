590c4e736cde497fe923ab9a1efa8e85ea865a35
NComGit <ncomthrowaway@protonmail.com>
2023-11-01 17:20:21 -0400

    Fixed button; Will now disappear if no more events are available

--- PlaylistPalooza/javascript/index.js
+++ PlaylistPalooza/javascript/index.js
@@ -3,32 +3,30 @@ let loadMoreBtn = document.getElementById("loadMoreBtn");
 loadMoreBtn.addEventListener("click", loadMoreRequest);
 
 let getLoadLimit = 3
-
-let setLoadLimit = JSON.stringify({
-    limit: getLoadLimit
-});
-
-let httpRequest;
+let httpRequest_loadEvents;
 
 function loadMoreRequest() {
-    httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = loadMoreHandler;
-    httpRequest.open("POST", "http://localhost/FSD_webdev1_Project/PlaylistPalooza/PlaylistPalooza/includes/loadMore.php", true);
-    httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    httpRequest.send("limit=" + getLoadLimit);
+    httpRequest_loadEvents = new XMLHttpRequest();
+    httpRequest_loadEvents.onreadystatechange = loadMoreHandler;
+    httpRequest_loadEvents.open("POST", "http://localhost/FSD_webdev1_Project/PlaylistPalooza/PlaylistPalooza/includes/loadMore.php", true);
+    httpRequest_loadEvents.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    httpRequest_loadEvents.send("limit=" + getLoadLimit);
     getLoadLimit += 3;
 }
 
 function loadMoreHandler() {
     // data from loadMore.php
 
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest_loadEvents.readyState === XMLHttpRequest.DONE) {
         // Everything is good, the response was received.
 
-        let response = httpRequest.response
+        let response = httpRequest_loadEvents.response;
 
-        document.getElementById("eventSection").innerHTML += (response);
+        if (response == "") {
+            loadMoreBtn.style.display = "none";
+        } else {
+            document.getElementById("eventSection").innerHTML += (response);
+        }
     }
-
 }
 
