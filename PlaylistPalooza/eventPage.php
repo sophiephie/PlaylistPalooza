ed628a3ea0d9f9079833ae39bc019ae110aba085
NComGit <ncomthrowaway@protonmail.com>
2023-11-05 14:51:20 -0500

    suggested events are now fetched from db by same genre

--- PlaylistPalooza/eventPage.php
+++ PlaylistPalooza/eventPage.php
@@ -10,7 +10,7 @@ if (!isset($_GET['item'])) {
 // there is a $_GET['item'] that exists
 $sql =
     "SELECT 
-        e.eventId, 
+        e.eventId as eventId, 
         e.mainArtistId, 
         mart.artistName as mainName, 
         e.openerArtistId, 
@@ -22,7 +22,8 @@ $sql =
         e.price,  -- Add ticket price to the query
         mart.imageLink,
         SUM(o.ticketQuantity) as ticketsSold,
-        l.maxCapacity as maxCapacity
+        l.maxCapacity as maxCapacity,
+        e.musicalGenre as musicalGenre
     FROM events AS e 
     LEFT JOIN artist AS mart ON mart.artistId = e.mainArtistId 
     LEFT JOIN artist AS oart ON oart.artistId = e.openerArtistId
@@ -38,10 +39,9 @@ if (!$result) { // nothing found in the database
     pageNotFound();
 }
 
-$mainArtistName = " Main Artist Name";
-$openerArtistName = "with " . "Opening Act";
-$eventLocation = "Place Bell Laval";
-$eventDate = "November 21, 2023";
+$eventGenre = $result['musicalGenre'];
+$eventPageId = $result['eventId'];
+
 ?>
 
 <!DOCTYPE html>
@@ -50,12 +50,11 @@ $eventDate = "November 21, 2023";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css" />
     <link rel="stylesheet" href="css/eventPage.css" />
     <title>
-        <?= $mainArtistName . " | Playlist Palooza" ?>
+        <?= $result['mainName']  . " | Playlist Palooza" ?>
     </title>
 </head>
 
@@ -86,8 +85,7 @@ $eventDate = "November 21, 2023";
                 <?php if ($result['ticketsSold'] >= $result['maxCapacity']) { ?>
                     <button type="button" class="btn btn-light">Sold Out</button>
                 <?php } else { ?>
-                    <a href="checkout.php?event=<?= $result['eventId'] ?>"><button type="button"
-                            class="btn btn-dark">Purchase Tickets</button></a>
+                    <a href="checkout.php?event=<?= $result['eventId'] ?>"><button type="button" class="btn btn-dark">Purchase Tickets</button></a>
                 <?php } ?>
             </section>
 
@@ -95,34 +93,46 @@ $eventDate = "November 21, 2023";
 
         <article class="otherEvent row container text-start">
             <section class="otherEvents row">
+
                 <h2>You may also like</h2>
-                <figure class="col">
-                    <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863"
-                            alt="artist name" width="100%"></a>
-                    <figcaption>
-                        <h4>Laval</h4>
-                        <h2>Charlotte Cardin</h2>
-                        <h4>Feb 8 2023</h4>
-                    </figcaption>
-                </figure>
-                <figure class="col">
-                    <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863"
-                            alt="artist name" width="100%"></a>
-                    <figcaption>
-                        <h4>Laval</h4>
-                        <h2>Charlotte Cardin</h2>
-                        <h4>Feb 8 2023</h4>
-                    </figcaption>
-                </figure>
-                <figure class="col">
-                    <a href=#> <img src="https://www.evenko.ca/_uploads/event/57640/featured.jpg?v=1696253863"
-                            alt="artist name" width="100%"></a>
-                    <figcaption>
-                        <h4>Laval</h4>
-                        <h2>Charlotte Cardin</h2>
-                        <h4>Feb 8 2023</h4>
-                    </figcaption>
-                </figure>
+
+                <?php
+                $sql =
+                    "SELECT 
+                        e.eventId as eventId, 
+                        e.mainArtistId, 
+                        mart.artistName as mainName, 
+                        e.openerArtistId, 
+                        oart.artistName as openName,
+                        e.location_Id,
+                        l.locationName as locationName,
+                        e.date as eventDate,
+                        mart.imageLink as imageLink
+                    FROM events AS e 
+                    LEFT JOIN artist AS mart ON mart.artistId = e.mainArtistId 
+                    LEFT JOIN artist AS oart ON oart.artistId = e.openerArtistId
+                    LEFT JOIN orders AS o ON o.eventId = e.eventId
+                    JOIN locations as l on l.locationId = e.location_Id
+                    WHERE e.musicalGenre = :genre and e.eventId != :id
+                    GROUP BY e.eventId
+                    LIMIT 3";
+
+                $query = $db->prepare($sql);
+                $query->execute(['genre' => $eventGenre, 'id' => $eventPageId]);
+
+                while ($row = $query->fetch()) {
+                    $link = "eventPage.php?item=" . $row['eventId'];
+                ?>
+                    <figure class="col">
+                        <a href=<?= $link ?>> <img src=<?= $row['imageLink'] ?> alt="artist name" width="100%"></a>
+                        <figcaption>
+                            <h4><?= $row['locationName'] ?></h4>
+                            <h2><?= $row['mainName'] ?></h2>
+                            <h3><?= "with " .  $row['openName'] ?></h3>
+                            <h4><?= date('F j, Y', strtotime($row['eventDate'])) ?> </h4>
+                        </figcaption>
+                    </figure>
+                <?php } ?>
             </section>
         </article>
     </article>
@@ -131,9 +141,7 @@ $eventDate = "November 21, 2023";
         <?php include "includes/footer.html"; ?>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
