7ec0d467606eeb468318c99a4e8210ef01e0fe59
Bo Kyung Kim <you@example.com>
2023-11-06 14:08:20 -0500

    updated event delete function

--- PlaylistPalooza/delete_events.php
+++ PlaylistPalooza/delete_events.php
@@ -6,11 +6,28 @@ include "includes/dbConnect.php";
 if (isset($_GET['id'])) {
     $eventId = $_GET['id'];
 
+    $db->beginTransaction();
+
+    // Delete associated orders first
+    $sqlDeleteOrders = "DELETE FROM orders WHERE eventId = :eventId";
+    $stmtDeleteOrders = $db->prepare($sqlDeleteOrders);
+    $stmtDeleteOrders->bindParam(':eventId', $eventId);
+    $stmtDeleteOrders->execute();
+
+    // Delete associated tickets
+    $sqlDeleteTickets = "DELETE FROM tickets WHERE event_Id = :eventId";
+    $stmtDeleteTickets = $db->prepare($sqlDeleteTickets);
+    $stmtDeleteTickets->bindParam(':eventId', $eventId);
+    $stmtDeleteTickets->execute();
+
     // Delete event data from the database
     $sql = "DELETE FROM events WHERE eventId = :eventId";
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':eventId', $eventId);
     $stmt->execute();
+
+    // Commit the transaction
+    $db->commit();
 
     // Redirect back to the events management page
     header("Location: eventsPanel.php");
